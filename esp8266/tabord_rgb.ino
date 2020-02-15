14245c7930ab98438a266f2ed4913840f76940f1
Qandra Si <qandra.si@gmail.com>
2020-02-15 04:39:50 +0000

    первая версия (рабочая) с WPS и сохранением SSI и пароля в EEPROM

--- esp8266/tabord_rgb.ino
+++ esp8266/tabord_rgb.ino
@@ -1,121 +1,146 @@
+#include <ESP8266WiFi.h>
+#include <EEPROM.h>
 
-/*
-  Web client
+const char * g_ssid = NULL;
+const char * g_pswd = NULL;
+byte g_eeprom_data[512];
 
-  This sketch connects to a website (http://www.google.com)
-  using a WiFi shield.
+void readEEPROM();
+void parseEEPROM();
+void renewEEPROM(const char * ssid, const char * pswd);
+bool startWPS();
 
-  This example is written for a network using WPA encryption. For
-  WEP or WPA, change the Wifi.begin() call accordingly.
-
-  This example is written for a network using WPA encryption. For
-  WEP or WPA, change the Wifi.begin() call accordingly.
-
-  Circuit:
-   WiFi shield attached
-
-  created 13 July 2010
-  by dlf (Metodo2 srl)
-  modified 31 May 2012
-  by Tom Igoe
-*/
-
-
-#include <SPI.h>
-#include <WiFi.h>
-
-char ssid[] = "bsrgin"; //  your network SSID (name)
-char pass[] = "********";    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
-
-int status = WL_IDLE_STATUS;
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "www.google.com";    // name address for Google (using DNS)
-
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-WiFiClient client;
-
-void setup() {
-  //Initialize serial and wait for port to open:
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+  EEPROM.begin(512);
+
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level)
+                                    // but actually the LED is on; this is because
+                                    // it is active low on the ESP-01)
+
+  Serial.println("\nReading SSID and password from EEPROM ...");
+  readEEPROM();
+  parseEEPROM();
+  if (g_ssid && g_pswd)
+  {
+    Serial.printf("EEPROM read. SSID '%s' and password '%s'\n", g_ssid, g_pswd);
+    WiFi.begin(g_ssid, g_pswd);
+  }
+  else // if (g_ssid == null || g_pswd == null)
+  {
+    Serial.println("EEPROM empty or corrupted.\nPress WPS button on your router ...");
+    delay(5000);
+    if (startWPS())
+    {
+      parseEEPROM();
+      if (g_ssid && g_pswd)
+      {
+        Serial.printf("WPS finished sucessfully. SSID '%s' and password '%s'\n", g_ssid, g_pswd);
+      }
+    }
   }
 
-  // check for the presence of the shield:
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    // don't continue:
-    while (true);
-  }
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+}
 
-  String fv = WiFi.firmwareVersion();
-  if (fv != "1.1.0") {
-    Serial.println("Please upgrade the firmware");
-  }
+void readEEPROM()
+{
+  for (int i = 0; i < 512; ++i)
+    g_eeprom_data[i] = EEPROM.read(i);
+}
 
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-  Serial.println("Connected to wifi");
-  printWifiStatus();
-
-  Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
-    client.println("Connection: close");
-    client.println();
+void parseEEPROM()
+{
+  // max lengths | 0 ... 31 | '\0' | 33 .. 96 | '\0' |
+  g_ssid = NULL;
+  g_pswd = NULL;
+  if (g_eeprom_data[0] == '\0') return;
+  for (int i = 1; i <= 32; ++i) // max ssid len = 32
+  {
+    if (g_eeprom_data[i] == '\0')
+    {
+      if (i == 1)
+      {
+        Serial.println("ERROR! SSID corrupted");
+        break;
+      }
+      g_ssid = (const char*)g_eeprom_data;
+      Serial.printf("SSID '%s' found\n", g_ssid);
+      for (int j = i + 1; j < i + 64; ++j) // max pswd len = 64
+      {
+        if (g_eeprom_data[j] == '\0')
+        {
+          if (j == (i + 1))
+          {
+            Serial.println("ERROR! Password corrupted");
+            break;
+          }
+          g_pswd = (const char*)&g_eeprom_data[i+1];
+          Serial.printf("Password '%s' found\n", g_pswd);
+          break;
+        }
+      }
+      break;
+    }
   }
 }
 
-void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
+void renewEEPROM(const char * ssid, const char * pswd)
+{
+  int len_ssid = strlen(ssid);
+  int len_pswd = strlen(pswd);
+  if (len_ssid && len_pswd && (len_ssid <= 32) && (len_pswd <= 64))
+  {
+    strcpy((char*)g_eeprom_data, ssid);
+    g_eeprom_data[len_ssid] = '\0';
+    strcpy((char*)&g_eeprom_data[len_ssid+1], pswd);
+    g_eeprom_data[len_ssid+1+len_pswd] = '\0';
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
-
-    // do nothing forevermore:
-    while (true);
+    for (int i = 0, cnt = len_ssid+1+len_pswd+1; i < cnt; ++i)
+      EEPROM.write(i, g_eeprom_data[i]);
+    if (EEPROM.commit())
+      Serial.println("EEPROM successfully committed");
+    else
+      Serial.println("ERROR! EEPROM commit failed");
   }
 }
 
+// see https://github.com/esp8266/Arduino/issues/1958#issue-150097589
+bool startWPS()
+{
+  Serial.println("WPS config start");
+  // WPS works in STA (Station mode) only -> not working in WIFI_AP_STA !!! 
+  WiFi.mode(WIFI_STA);
+  delay(1000);
+  WiFi.begin("foobar",""); // make a failed connection
+  while (WiFi.status() == WL_DISCONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  bool wpsSuccess = WiFi.beginWPSConfig();
+  if (wpsSuccess)
+  {
+      // Well this means not always success :-/ in case of a timeout we have an empty ssid
+      String newSSID = WiFi.SSID();
+      if (newSSID.length() > 0)
+      {
+        // WPSConfig has already connected in STA mode successfully to the new station. 
+        Serial.printf("WPS finished. Connected successfull to SSID '%s' with '%s'", newSSID.c_str(), WiFi.psk().c_str());
+        // save to config and use next time or just use - WiFi.begin(WiFi.SSID().c_str(),WiFi.psk().c_str());
+        renewEEPROM(newSSID.c_str(), WiFi.psk().c_str());
+      }
+      else
+      {
+        wpsSuccess = false;
+      }
+  }
+  return wpsSuccess; 
+}
 
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+void loop()
+{
+  // put your main code here, to run repeatedly:
 }
