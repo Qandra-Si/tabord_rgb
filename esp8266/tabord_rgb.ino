768a56257dc5fc614ddee0438a302086fafe995d
Qandra Si <qandra.si@gmail.com>
2020-02-15 01:47:25 +0000

    проверка работоспособнсти Амперка' Wi-Fi Troyka модуля

--- esp8266/tabord_rgb.ino
+++ esp8266/tabord_rgb.ino
@@ -1,80 +1,121 @@
-    // пины подключения RGB светодиодной ленты через ключи
-    #define LED_RED_PIN     10
-    #define LED_GREEN_PIN   9
-    #define LED_BLUE_PIN    11
 
-    #define FADE_SPEED      10
-    #define KILLMAILS_SIZE  3
+/*
+  Web client
 
-    unsigned long killMails[KILLMAILS_SIZE] = {
-      0x00fff200ul,
-      0x0000ff00ul,
-      0x00ff0000ul,
-    };
-     
-    void setup()
-    {
-      // пины в режим выхода
-      pinMode(LED_RED_PIN, OUTPUT);
-      pinMode(LED_GREEN_PIN, OUTPUT);
-      pinMode(LED_BLUE_PIN, OUTPUT);
-      pinMode(LED_BUILTIN, OUTPUT);
-    }
-     
-    void loop()
-    {
-        for (int k = 0; k < KILLMAILS_SIZE; ++k)
-        {
-          unsigned long red   = (killMails[k] & 0x00ff0000ul) >> 16;
-          unsigned long green = (killMails[k] & 0x0000ff00ul) >> 8;
-          unsigned long blue  =  killMails[k] & 0x000000fful;
-          for (int i=1;i<256; i++)
-          {
-            float power = (float)i / 255.0;
-            analogWrite(LED_RED_PIN, power * red);
-            analogWrite(LED_GREEN_PIN, power * green);
-            analogWrite(LED_BLUE_PIN, power * blue);
-            delay(FADE_SPEED);
-          }
-          for (int i = 255; i >= 1; --i)
-          {
-            float power = (float)i / 255.0;
-            analogWrite(LED_RED_PIN, power * red);
-            analogWrite(LED_GREEN_PIN, power * green);
-            analogWrite(LED_BLUE_PIN, power * blue);
-            delay(FADE_SPEED);
-          }
-        }
-      /*
-        digitalWrite(LED_GREEN_PIN, HIGH);   //включить зеленый светодиод на максимум
-        digitalWrite(LED_RED_PIN, HIGH);    // включить красный светодиод на максимум
-        digitalWrite(LED_BLUE_PIN, HIGH);   // включить синий светодиод на максимум
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(1000);                       // подождать 5 секунд
-        digitalWrite(LED_GREEN_PIN, LOW);  // выключаем зеленый светодиод
-        digitalWrite(LED_RED_PIN, LOW); // выключаем красный светодиод
-        digitalWrite(LED_BLUE_PIN, LOW);// выключаем синий светодиод
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(1000);
+  This sketch connects to a website (http://www.google.com)
+  using a WiFi shield.
 
-        // цикл, чтобы зажечь светодиод со скоростью затухания
-        for (int i=0;i<256; i++)
-        {
-            analogWrite(LED_RED_PIN, i);
-            delay(FADE_SPEED);
-        }
-        digitalWrite(LED_RED_PIN, LOW);  // выключаем зеленый светодиод
-        for (int i=0;i<256; i++)
-        {
-            analogWrite(LED_GREEN_PIN, i);
-            delay(FADE_SPEED);
-        }
-        digitalWrite(LED_GREEN_PIN, LOW);  // выключаем зеленый светодиод
-        for (int i=0;i<256; i++)
-        {
-            analogWrite(LED_BLUE_PIN, i);
-            delay(FADE_SPEED);
-        }
-        digitalWrite(LED_BLUE_PIN, LOW);  // выключаем зеленый светодиод
-    */
-    }
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the Wifi.begin() call accordingly.
+
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the Wifi.begin() call accordingly.
+
+  Circuit:
+   WiFi shield attached
+
+  created 13 July 2010
+  by dlf (Metodo2 srl)
+  modified 31 May 2012
+  by Tom Igoe
+*/
+
+
+#include <SPI.h>
+#include <WiFi.h>
+
+char ssid[] = "bsrgin"; //  your network SSID (name)
+char pass[] = "********";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "www.google.com";    // name address for Google (using DNS)
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv != "1.1.0") {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  Serial.println("Connected to wifi");
+  printWifiStatus();
+
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  }
+}
+
+void loop() {
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
